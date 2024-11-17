cd0aa1bf29fd2cc8523b7b433af91193116b15ea
René Tan <165171058+hackematrix@users.noreply.github.com>
2024-11-17 17:16:03 +0800

    Create prime_factor.cpp

---
+++ chapter1(1-50)/third/prime_factor.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+#define maxn 1010
+int a[maxn];
+int isprime(int n){
+  int i;
+  if (n==1)
+    return 0;
+  else 
+   for(i=2;i*i<=n;i++)
+    if(n%i==0)
+      return 0;
+  return 1; 
+}
+int find_max(int b[],int n){
+  int max=0;
+  int i;
+  for(i=0;i<n;i++){
+    if(b[i]>b[max])
+      max=i;
+  }
+  return max;
+}
+
+int main(){
+  int i;
+  int k=0;
+  int result;
+ long long int num=600851475143;    
+  for(i=2;i<=num;i++){
+    if(isprime(i)&&num%i==0)
+      {
+      a[k++]=i;
+      num/=i;
+    }
+  }
+  result=find_max(a,k);
+  cout<<a[result]<<endl;
+  return 0;
+
+}
